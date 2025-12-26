1ec260ea3aa7bf659a7637aa0c2b76366bacf4d4
user113333 <tevz.8bajzelj@gmail.com>
2025-12-26 14:31:08 +0100

    Add -c flag

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+var cFlag = flag.Bool("c", false, "on exit print cwd")
+
 func main() {
+	var model Model
+
+	flag.Parse()
 	p := tea.NewProgram(ModelNew(), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
+	m, err := p.Run()
+	if err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
+	}
+
+	model = m.(Model)
+
+	if *cFlag {
+		fmt.Printf("%s\n", model.cwd)
 	}
 }
